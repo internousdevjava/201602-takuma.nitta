1a52df8901c39bd6dd02aaee25ee62c0918150a5
Takuma Nitta <tetuno@hotmail.co.jp>
2016-02-18 12:57:11 +0900

    基礎課題2(修正①)仁田2/18 12:57 よろしくお願いします

--- src/KisoKadai2.java
+++ src/KisoKadai2.java
@@ -11,7 +11,7 @@ class KisoKadai2 {
     	Random rand = new Random();
         //ランダムとスキャナのクラスインスタンス化
 
-        System.out.println("数当てゲームver1.0を開始します");
+        System.out.println("数当てゲームver1.1を開始します");
         int answer = rand.nextInt(100) + 1;
         //ランダムで1～100の数を定義
 
@@ -29,14 +29,33 @@ class KisoKadai2 {
                     try{
                         str = br.readLine();
                         int a=Integer.parseInt(str);
-            if (answer > a){
+            if (answer > a && a >= 1){
                 System.out.println("もっと大きい数です");
-            } else if(answer < a){
+            } else if(answer < a && a <= 100){
                 System.out.println("もっと小さい数です");
-            } else {
+            } else if(answer == a){
             	System.out.println("正解です……");
-            	break;
-            }
+
+        		System.out.println("続けますか？ yes→1 no→2");
+         		str = br.readLine();
+
+         		int c=Integer.parseInt(str);
+
+         		if(c == 1){
+         			System.out.println("継続します。1から100までの数を当てて下さい : ");
+         			continue;
+         		}else if(c == 2){
+         			System.out.println("終了します。お疲れ様でした");
+         			break;
+         		}else{
+         			System.out.println("無効な値が入力されました。終了します");
+         			break;
+         		}
+
+            }else{
+                	System.out.println("すみません、1～100の整数を入力してください");
+            	}
+
             }catch(Exception e){
               /*	e.printStackTrace();
     	        }if (yourAns == i){
@@ -46,10 +65,10 @@ class KisoKadai2 {
               	}else if(yourAns >= 101){
               	}*/
             	System.out.println("すみません、1～100の整数を入力してください");
-            }
+//
 
 
-        }}}
+                }}}}
 
 	// }
 	// Math.random()*100 if (0=ans){System.out.println("0");}
