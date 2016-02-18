9c87bbdbd41392ad68bf04df32bcdf833d061be0
Takuma Nitta <tetuno@hotmail.co.jp>
2016-02-18 18:34:00 +0900

    基礎課題3(修正①)仁田2/18 18:33 よろしくお願いします

--- src/KisoKadai3.java
+++ src/KisoKadai3.java
@@ -1,146 +1,97 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 
-public class KisoKadai3 {
+class KisoKadai3 {
 
 	public static void main(String[] args)throws IOException{
-		System.out.println("テキストファイル管理プログラムver1.0を開始します");
-		System.out.println("ファイルの新規作成は0,ファイルの読み込みは1,ファイルの追記は2,ファイルの上書きは3,終了は5を入力して下さい");
-    	String str = null;
         BufferedReader br = new BufferedReader
         		(new InputStreamReader(System.in));
+		FileWriter fw1 ;
+		String str1;
+		File file3;
+		// strは犠牲になったのだ
+		//fileも犠牲になったのだ
 
 
-
-        int x = 0;
+//
+		System.out.println("テキストファイル管理プログラムver1.1を開始します");
         //ループ用関数
-
+        int x = 0;
         while (x==0){
-        try{
-            str = br.readLine();
-            int a=Integer.parseInt(str);
+
+        		 try{
+
+
+
+
+		System.out.println("ファイルの新規作成は0,ファイルの上書きは1,ファイルの追記は2,ファイルの読み込みは3,終了は5を入力して下さい");
+
+
+            str1 = br.readLine();
+            int a=Integer.parseInt(str1);
 
             if (a == 0){
+
+
+            	file3 = nitta(nittaku(br),br);
             	System.out.println("ファイルの新規作成を行います");
             	System.out.println("作成するファイル名を入力して下さい");
 
             	//新規作成プログラムが入る
+            	fw1 = new FileWriter(file3);
+            	fw1.write(br.readLine());
+            	fw1.close();
+            	//filewriterはcloseしないと終わらないとのこと
+ 			    System.out.println("ファイルを作りました");
+ 			    totta();
 
-            	File file = new File(args[0]);
-            	File dir=new File(file.getParent());
-            	//ディレクトリパスなるものを取得したらしい
-
-//            	if(!dir.exists()){
-//            		//フォルダがなかった場合
-//            		System.out.println("フォルダがないので作成します" + file.getAbsolutePath());
-//            		//アブソリュートパスはフォルダの位置を表す
-//            		dir.mkdirs();
-//            		System.out.println("フォルダを作りました");
-//            	} else {
-//            		System.out.println("フォルダを見つけました");
-//            	//実行結果に関係なく下の処理へ
-//
-            	if(file.exists()){
-            		//ファイルがあった場合
-            		System.out.println("同名のファイルが既にあるみたいです\n" + file.getAbsolutePath());
-            	}else{
-            		System.out.println("同名のファイルはないみたいです\n" + file.getAbsolutePath());
-            		try{
-            			if (file.createNewFile()){
-            			   System.out.println("ファイルを作りました");
-            			}else{
-            			   System.out.println("作れなかったみたいです…");
-            			   //作れないってどういう状況…？
-            			}
-
-            		}catch(IOException e){
-            			System.out.println("エラーです");
-            		}
-            		}
-
-
-//            	}
-
-    			break;
 
             } else if(a == 1){
-            	System.out.println("ファイルの読み込みを行います");
-            	System.out.println("読み込むファイルを選択して下さい");
-
-            	File file = new File(args[0]);
-            	File dir=new File(file.getParent());
-            	//ディレクトリパスなるものを取得したらしい(2回目、ローカルにしないとどういうわけだかエラー)
-            	//読み込みプログラムが入る
-
-            	try{
-            		FileReader filereader = new FileReader(file.getAbsolutePath());
-            		//ファイルを読み込む
-
-            		int b;
-
-
-            	}catch(Exception e){
-            		System.out.println("エラーです");
-            	}
-
-            	break;
-
-
-            } else if(a == 2){
-            	System.out.println("ファイルの追記を行います");
-            	System.out.println("読み込むファイルを選択して下さい");
-
-            	File file = new File(args[0]);
-            	File dir=new File(file.getParent());
-            	//ディレクトリパスなるものを取得したらしい(3回目、ローカルにしないとどういうわけだかエラー)
-
-            	//読み込みプログラムが入る
-
-            	FileWriter fw = new FileWriter(file.getAbsolutePath(),true);
-            	PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
-            	//どうやらReaderは使わないらしい。trueは追記でfalseは上書きになる
-
-            	BufferedReader br3 = new BufferedReader(new InputStreamReader(System.in));
-            	String str3 = br3.readLine();
-            	pw.println(str3);
-            	//？？？？？
-
-            	pw.close();
-            	System.out.println("ファイルの追記が完了しました");
-
-
-            	break;
-
-            } else if(a == 3){
+            	file3 = takuma(nittaku(br),br);
             	System.out.println("ファイルの上書きを行います");
             	System.out.println("読み込むファイルを選択して下さい");
 
-            	File file = new File(args[0]);
-            	File dir=new File(file.getParent());
-            	//ディレクトリパスなるものを取得したらしい(3回目、ローカルにしないとどういうわけだかエラー)
+            	//読み込みプログラムが入る
+            	fw1 = new FileWriter(file3);
+            	fw1.write(br.readLine());
+            	fw1.close();
+            	System.out.println("ファイルの上書きが完了しました");
+            	totta();
+
+
+
+            } else if(a == 2){
+            	file3 = takuma(nittaku(br),br);
+            	System.out.println("ファイルの追記を行います");
+            	System.out.println("読み込むファイルを選択して下さい");
 
             	//読み込みプログラムが入る
-
-            	FileWriter fw = new FileWriter(file.getAbsolutePath(),false);
-            	PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
-            	//どうやらReaderは使わないらしい。trueは追記でfalseは上書きになる
-
-            	BufferedReader br3 = new BufferedReader(new InputStreamReader(System.in));
-            	String str3 = br3.readLine();
-            	pw.println(str3);
-            	//？？？？？
-
-            	pw.close();
-            	System.out.println("ファイルの上書きが完了しました");
+            	fw1 = new FileWriter(file3,true);
+            	fw1.write(br.readLine());
+            	fw1.close();
+            	System.out.println("ファイルの追記が完了しました");
+            	totta();
 
 
-            	break;
+            } else if(a == 3){
+            	file3 = takuma(nittaku(br),br);
+            	System.out.println("ファイルの読み込みを行います");
+            	System.out.println("読み込むファイルを選択して下さい");
+
+
+            	//読み込みプログラムが入る
+            	FileReader fr = new FileReader(file3);
+            	int ch;
+            	while((ch = fr.read()) != -1){
+            		System.out.print((char)ch);
+            		//intからchar型に変換。一文字ずつ表示して全部表示しきると-1が戻り値になるためループ終了
+            	}
+            	System.out.println("");
+            	totta();
 
             } else if(a == 4){
             	System.out.println("4は縁起が悪いので使いません");
@@ -149,20 +100,132 @@ public class KisoKadai3 {
 			} else if(a == 5){
 				System.out.println("終了します");
 				System.out.println("お疲れ様でした");
-
-            	break;
+				System.exit(0);
+				//System.exit(0);で終了。知らんかった
 
 			} else {
 				System.out.println("ごめんなさい、0,1,2,3,5のいずれかを入力してください");
 			}
+
 			}catch(Exception e){
 			  /*	e.printStackTrace();
 
 			  	}*/
-				System.out.println("すみません、0,1,2,3,5のいずれかを入力してください");
+				System.out.println("エラーです。メニューに戻ります");
+				//ざっくり
+				continue;
+			}
+        }
+	}
 
+
+	private static File takuma(String str, BufferedReader br) throws IOException{
+			String str2 =str;
+			String str3;
+			String str4;
+			File file2;
+
+			while(true){
+            	System.out.println("ファイルの読み込みを行います");
+            	System.out.println("読み込むファイルを選択して下さい");
+            	str3 = br.readLine();
+            	str4 = str2 + "\\" + str3;
+            	//バックスラッシュが出ないクポ
+            	file2 = new File(str4);
+            		if(file2.exists()){
+            			System.out.println("そのファイルは既に存在しています");
+            		}else{
+            			file2.createNewFile();
+            			System.out.println("ファイルを新規作成します");
+            			break;
+            		}
 
 			}
+			return file2;
+	}
+
+	private static File nitta(String str, BufferedReader br) throws IOException {
+			String str2 =str;
+			String str3;
+			String str4;
+			File file2;
+
+			while(true){
+            	System.out.println("ファイルの読み込みを行います");
+            	System.out.println("読み込むファイルを選択して下さい");
+            	str3 = br.readLine();
+            	str4 = str2 + "\\" + str3;
+            	//バックスラッシュが出ないクポ
+            	file2 = new File(str4);
+            		if(file2.exists()){
+            			System.out.println("そのファイルは既に存在しています");
+            		}else{
+            			file2.createNewFile();
+            			System.out.println("ファイルを新規作成します");
+            			break;
+            		}
+
+			}
+			return file2;
+	}
+			private static String nittaku(BufferedReader br) throws IOException {
+				String str1;
+				String str2 = null;
+				File file1;
+
+				System.out.println("フォルダ名を入力して下さい");
+
+				int q = 0;
+				while(q < 1){
+					str2 = br.readLine();
+					file1 = new File(str2);
+					if(!file1.exists()){
+
+
+	            	System.out.println("フォルダが存在しません。作成してもよろしいですか？ はい=a いいえ=b");
+	            	while(true){
+	            		str1 = br.readLine();
+	            		String a = "a", b = "b";
+	            		if(str1.equals(a)){
+	            			file1.mkdirs();
+
+	            			System.out.println("作成しました");
+	            			q++;
+	            			break;
+	            		}else if(str1 .equals(b)){
+	            				 System.out.println("終了します。お疲れ様でした");
+	            			     System.exit(0);
+	            		}else{
+	            			System.out.println("申し訳ありません、aかbで入力して下さい");
+	            		}
+	            	}
+
+
+	            		}else{
+
+	            			break;
+	            		}
+
+				}
+				return str2;
+			}
+			private static void totta() throws IOException {
+				System.out.println("継続して作業を行いますか？ はい=a いいえ=b");
+				BufferedReader br = new BufferedReader(new InputStreamReader(System. in));
+
+        		String a = "a", b = "b";
+            	while(true){
+            		String toto = br.readLine();
+            		if(toto.equals(a)){
+            			System.out.println("メニューに戻ります");
+            			break;
+            		}else if(toto .equals(b)){
+            				 System.out.println("終了します。お疲れ様でした");
+            			     System.exit(0);
+            		}else{
+            			System.out.println("申し訳ありません、aかbで入力して下さい");
+
+            		}
          //try
         	}
         //while
