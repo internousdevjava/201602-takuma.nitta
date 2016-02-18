ac80e1d02dcf23dd509bd9324267f1935f59c24b
Takuma Nitta <tetuno@hotmail.co.jp>
2016-02-18 11:39:21 +0900

    基礎課題1(修正①)仁田2/18 11:39 よろしくお願いします

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -7,9 +7,12 @@ public class KisoKadai1
 {
     public static void main(String[] args) throws IOException{
     	//メインメソッドの宣言
-    	System.out.println("\n九九表ver1.0\n\n[x軸とy軸を調整できます]\n---\n");
+    	System.out.println("\n九九表ver1.1\n\n[x軸とy軸を調整できます]\n---\n");
 
 
+        int i = 0;
+
+        while (i==0){
         try{
 
       		String str = null;
@@ -39,7 +42,26 @@ public class KisoKadai1
                 			System.out.print((x*y) + " ");
             			}
                 		System.out.println();
-		}   }catch(Exception e){
+
+
+
+		}
+
+            		System.out.println("続けますか？ yes→1 no→2");
+             		str = br.readLine();
+
+             		int c=Integer.parseInt(str);
+
+             		if(c == 1){continue;
+             		}else if(c == 2){
+             			System.out.println("終了します。お疲れ様でした");
+             			break;
+             		}else{
+             			System.out.println("無効な値が入力されました。終了します");
+             			break;
+             		}
+
+        }catch(Exception e){
             /*	e.printStackTrace();
   	        }if (yourAns == i){
             		System.out.println("数字を入力してください");
@@ -48,6 +70,7 @@ public class KisoKadai1
             	}else if(yourAns >= 101){
             	}*/
           	System.out.println("すみません、正の整数を入力してください");
+		}
           }
 
             }
