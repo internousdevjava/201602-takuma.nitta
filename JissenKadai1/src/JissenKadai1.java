681f7caf248e601c4023b580e87b4f667517c278
Takuma Nitta <tetuno@hotmail.co.jp>
2016-02-23 14:57:10 +0900

    実践課題

---
+++ JissenKadai1/src/JissenKadai1.java
@@ -0,0 +1,16 @@
+
+import com.mysql.jdbc.Driver;
+
+public class JissenKadai1 {
+  public static void main(String[] args) {
+        String msg = "";
+        try {
+        	Driver d = new Driver();
+
+        	System.out.println("JDBCに接続できました！");
+        }catch (Exception e){
+            msg = "JDBC接続に失敗しました";
+            System.out.println(msg);
+        }
+  }
+}
